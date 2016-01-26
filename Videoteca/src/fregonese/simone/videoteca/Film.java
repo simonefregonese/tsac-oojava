dbd741bbd421fc192c3dfc05b1d4630fb21c025a
Simone Fregonese <Simone Fregonese@192.168.101.129>
2016-01-26 09:30:22 +0100

    26.01.2016

---
+++ Videoteca/src/fregonese/simone/videoteca/Film.java
@@ -0,0 +1,24 @@
+package fregonese.simone.videoteca;
+
+public class Film {
+
+	String titolo;
+	String regista;
+	int anno;
+	String attori;
+	
+	
+	/*int calcoloNoleggio(){
+		
+		 cassetta:
+1 euro il primo giorno, dal secondo giorno in poi 2 euro al giorno;
+dvd:
+1.50 euro al giorno per i primi 2 giorni, poi dal terzo giorno 2 euro;
+bluray:
+1 euro il primo giorno, poi c’è una penale del 10% in più per ogni giorno
+successivo. In questo caso la penale si somma al costo del noleggio del giorno
+precedente, ovvero: CostoNoleggio t+1 = CostoNoleggio t + CostoNoleggio t * Penale
+con t indicante il periodo in giorni.
+		 
+	}*/
+}
